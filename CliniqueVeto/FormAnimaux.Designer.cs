3e31ca0d5c854d6a4356cb6430d93f70e703a5a0
JulieLH <julie.lherault@rocketmail.com>
2015-02-02 17:26:55 +0100

    * Push Final du 02/02 *
    Signed-off-by: JulieLH <julie.lherault@rocketmail.com>

--- CliniqueVeto/FormAnimaux.Designer.cs
+++ CliniqueVeto/FormAnimaux.Designer.cs
@@ -29,15 +29,27 @@
         private void InitializeComponent()
         {
             this.Panel_GestionAnimaux = new System.Windows.Forms.Panel();
+            this.BTN_Dossier = new System.Windows.Forms.Button();
+            this.Panel_GestionAnimaux.SuspendLayout();
             this.SuspendLayout();
             // 
             // Panel_GestionAnimaux
             // 
             this.Panel_GestionAnimaux.BackColor = System.Drawing.SystemColors.Control;
+            this.Panel_GestionAnimaux.Controls.Add(this.BTN_Dossier);
             this.Panel_GestionAnimaux.Location = new System.Drawing.Point(0, 0);
             this.Panel_GestionAnimaux.Name = "Panel_GestionAnimaux";
             this.Panel_GestionAnimaux.Size = new System.Drawing.Size(472, 83);
             this.Panel_GestionAnimaux.TabIndex = 0;
+            // 
+            // BTN_Dossier
+            // 
+            this.BTN_Dossier.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BTN_Dossier.Location = new System.Drawing.Point(12, 12);
+            this.BTN_Dossier.Name = "BTN_Dossier";
+            this.BTN_Dossier.Size = new System.Drawing.Size(50, 50);
+            this.BTN_Dossier.TabIndex = 0;
+            this.BTN_Dossier.UseVisualStyleBackColor = true;
             // 
             // FormAnimaux
             // 
@@ -49,6 +61,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "FormAnimaux";
             this.Text = "Animaux";
+            this.Panel_GestionAnimaux.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Panel Panel_GestionAnimaux;
+        private System.Windows.Forms.Button BTN_Dossier;
 
     }
 }
