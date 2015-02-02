595a9fcab4e3bc430314be4a38b03207413b9e99
ClementREGY <clement.regy@gmail.com>
2015-02-02 11:42:53 +0100

    * Acces à la BDD *
    Signed-off-by: ClementREGY <clement.regy@gmail.com>

---
+++ DAL/DALAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class DALAccess
+    {
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection cnx = new SqlConnection();
+            try
+            {
+                cnx.ConnectionString = ConfigurationManager.ConnectionStrings["ConnexionPrincipale"].ToString();
+                cnx.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Erreur à l'ouverture de la connexion");
+            }
+            return cnx;
+        }
+    }
+}
