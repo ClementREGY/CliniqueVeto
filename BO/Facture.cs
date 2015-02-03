d71aa1e301cc2c9756dcd5c9608cf865c1a8808e
ClementREGY <clement.regy@gmail.com>
2015-02-03 10:02:33 +0100

    * BO FACTURE 2e tentative *
    Signed-off-by: ClementREGY <clement.regy@gmail.com>

---
+++ BO/Facture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Facture
+    {
+        public Guid numFacture { get; set; }
+        public DateTime dateFacture { get; set; }
+        public Guid codeClient { get; set; }
+        public float totalFacture { get; set; }
+        public bool archive { get; set; }
+        public string etat { get; set; }
+        public string rappelEnvoye { get; set; }
+
+        public Facture()
+        {
+        }
+
+        public Facture(Guid NumFacture, DateTime DateFacture, Guid CodeClient, float TotalFacture, bool Archive,
+            string Etat, string RappelEnvoye)
+        {
+            this.numFacture = NumFacture;
+            this.dateFacture = DateFacture;
+            this.codeClient = CodeClient;
+            this.totalFacture = TotalFacture;
+            this.archive = Archive;
+            this.etat = Etat;
+            this.rappelEnvoye = RappelEnvoye;
+        }
+    }
+}
